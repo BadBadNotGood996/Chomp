0ef37304f55f211c58431e8e82746615caddb010
vlad <vlad.96@live.it>
2024-04-24 18:24:06 +0200

    matrix implementation

--- script.js
+++ script.js
@@ -1,45 +1,95 @@
 const grid = document.querySelector('.grid')
 
-
 const gridLayout = 4
-const gridArray = []
+const matrix = []
 
-grid.style.gridTemplateColumns = `repeat(${gridLayout}, 1fr)`
-
-function createBiscuit (n) {
-    const img = document.createElement('img');
-    img.className = `biscuit ${n} ${biscuitClassName(n)}`;
-    img.src = 'images/biscuit.png'
-    return img
-}
-
-function biscuitClassName (n) {
-    const rowA = [0, 4, 8, 12]
-    const rowB = [1, 5, 9, 13]
-    const rowC = [2, 6, 10, 14]
-
-    if (rowA.includes(n)) {
-        return 'A';
-    } else if (rowB.includes(n)) {
-        return 'B';
-    } else if (rowC.includes(n)) {
-        return 'C';
-    } else {
-        return 'D'
+const createMatrix = function (n, m) {
+    for (let i = 0; i < n; i++) {
+        matrix.push([]);
+        for (let j = 0; j < m; j++) {
+            matrix[i].push(0);
+        }
     }
 }
 
-function createGrid (n) {
-    n = n * n
-    for (let i = 0; i < n; i++) {
+createMatrix(gridLayout, gridLayout);
+
+const matrixIndex = function (index) {
+    if (index < 4) {
+        return `0${index}`
+    } else if (index < 8) {
+        return `1${index - 4}`
+    } else if (index < 12) {
+        return `2${index - 8}`
+    } else if (index < 16) {
+        return `3${index - 12}`
+    }
+}
+
+grid.style.gridTemplateColumns = `repeat(${gridLayout}, 1fr)`
+
+function createBiscuit (index) {
+    const biscuit = document.createElement('img');
+    biscuit.className = `biscuit ${biscuitClassName(index)}`
+    biscuit.src = 'images/biscuit.png'
+    return biscuit
+}
+
+function biscuitClassName (index) {
+    return matrixIndex(index)
+}
+
+function createGrid (n=gridLayout, m=gridLayout) {
+    let index = n * m
+    for (let i = 0; i < index; i++) {
         grid.appendChild(createBiscuit(i))
-        gridArray.push(0)
     }
 }
 
 createGrid(gridLayout)
 
+// Logic
+grid.addEventListener('mouseover', function (e) {
+    if (e.target.classList.contains('biscuit')) {
+        const target = e.target
+        matrixOn(target)
+        matrix.forEach(el => console.log(el))
+    }
+})
 
+grid.addEventListener('mouseout', function (e) {
+    if (e.target.classList.contains('biscuit')) {
+        const target = e.target
+        matrixOff(target)
+        matrix.forEach(el => console.log(el))
+    }
+})
+
+const matrixOn = function (index, mtx=matrix) {
+    const newIndex = index.classList.value
+    const n = newIndex[8]
+    const m = newIndex[9]
+    for (let i = n; i < 4; i++) {
+        mtx[i][m] = 1
+        for (let j = m; j < 4; j++) {
+            mtx[i][j] = 1
+        }
+    }
+}
+
+const matrixOff = function (index, mtx=matrix) {
+    const newIndex = index.classList.value
+    const n = newIndex[8]
+    const m = newIndex[9]
+    for (let i = n; i < 4; i++) {
+        mtx[i][m] = 1
+        for (let j = m; j < 4; j++) {
+            mtx[i][j] = 0
+        }
+    }
+}
+
+/*
 grid.addEventListener('mouseover', function (e) {
     if (e.target.classList.contains('biscuit')) {
         const biscuit = e.target;
@@ -63,7 +113,6 @@ grid.addEventListener('click', function (e) {
 
 function hoverFx(el) {
     const biscuit = el
-    const biscuitCol = el.classList[2]
     const currentPos = Number.parseInt(el.classList[1])
 
     gridArray[currentPos] = 1
@@ -76,7 +125,6 @@ function hoverFx(el) {
 
 function hoverFxNo(el) {
     const biscuit = el
-    const biscuitCol = el.classList[2]
     const currentPos = Number.parseInt(el.classList[1])
 
     gridArray[currentPos] = 0
@@ -86,3 +134,4 @@ function hoverFxNo(el) {
         }
     })
 }
+*/
